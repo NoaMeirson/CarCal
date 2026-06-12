d9b98ecb0bac3da4728c7a129d70205bffdcdbfc
noame <noameir001@gmail.com>
2026-06-12 11:36:49 +0300

    endpoints settings

--- API/API_main.py
+++ API/API_main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["GET", "POST"],
     allow_headers=["Content-Type"],
 )
--- Client/ClientConfig.py
+++ Client/ClientConfig.py
@@ -2,4 +2,6 @@ API_URL = "http://localhost:8002/analyze"
 
 REQUEST_TIMEOUT = 60
 
-MODE= "combined"
+MODE= "car_parts_only"
+
+#mode options: "combined", "car_parts_only", "damage_only"
